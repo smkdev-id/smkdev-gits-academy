bb4f7d1d10f76501ae0bd18ac7e8ec228f6cd49d
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-07-07 17:05:30 +0700

    Add Header Content

--- smkdev-landing-page/src/components/Header.svelte
+++ smkdev-landing-page/src/components/Header.svelte
@@ -62,7 +62,7 @@
 </style>
 
 <header
-    class={"sticky z-[10] top-0 duration-200 lg:px-24 px-6 flex items-center justify-between border-b border-solid " +
+    class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
         (y > 0
             ? " py-4 bg-white border-violet-950"
             : " py-6 bg-transparent border-transparent")}
@@ -89,7 +89,7 @@
                 {#if showLearnDropdown}
                     <div class="absolute top-full mt-2 bg-white shadow-lg rounded-md">
                         {#each tab.dropdown as item}
-                            <a href={item.link} class="block px-4 py-2 text-indigo-700 hover:bg-violet-100">
+                            <a href={item.link} class="block  px-4 py-2 text-indigo-700 hover:bg-violet-100">
                                 {item.name}
                             </a>
                         {/each}
@@ -109,7 +109,7 @@
         {/each}
         {#each tabs as tab}
         {#if tab.name == "Dashboard"}
-        <a href={tab.link} class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-indigo-700 text-slate-950">
+        <a href={tab.link} class="blueShadow relative overflow-hidden px-5 py-2 group rounded-md bg-violet-900 text-slate-950">
             <div
                 class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
             />
@@ -134,9 +134,9 @@
     {#each tabs as tab, index}
         <div class="relative mb-2">
             {#if tab.name == "Learn"}
-                <p class="block px-4 py-2 text-indigo-700 hover:bg-violet-100" on:click={toggleMobileLearnDropdown}>
+                <a href="#/" class="block px-4 py-2 text-indigo-700 hover:bg-violet-100" on:click={toggleMobileLearnDropdown}>
                     {tab.name}
-                </p>
+                </a>
                 {#if showMobileLearnDropdown}
                     <div class="ml-4">
                         {#each tab.dropdown as item}
---
+++ smkdev-landing-page/src/components/Main.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Step from "./Step.svelte"
+    let slogan = [
+        {
+            name: "Jadilah Talenta Digital",
+            runTxt: "Masa Depan Indonesia",
+            description: "Belajar langsung dengan expert dari industri dengan kurikulum komperhensif berbasis project-based learning"
+        }
+    ]
+</script>
+
+<main class="flex flex-col flex-1 p-4 ">
+    <section
+    id="introPage"
+    class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+>
+    <div
+        class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    >
+        <h4 class="font-semibold text-4xl sm:text-4xl ">
+            {slogan[0].name}
+            <span class="poppins text-violet-400">{slogan[0].runTxt}</span>
+        </h4>
+        <p class="text-base sm:text-lg md:text-lg">
+            {slogan[0].description}
+        </p>
+    </div>
+    <div class="relative grid place-items-end">
+        <img
+            src={"images/profile.png"}
+            alt="Zetane Engine"
+            class="object-cover z-[2] max-h-[70vh]"
+        />
+    </div>
+</section>
+</main>
