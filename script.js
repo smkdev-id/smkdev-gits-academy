f609b70f6d2240fae454197336e4685e36e2fa8c
KONICCO <nico678514@gmail.com>
2024-07-08 23:28:50 +0700

    new landing page

---
+++ script.js
@@ -0,0 +1,117 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Animasi fade-in untuk setiap section
+    const sections = document.querySelectorAll('section');
+
+    const options = {
+        threshold: 0.5
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        section.classList.add('fade-in');
+        observer.observe(section);
+    });
+    AOS.init({
+        duration: 800,
+        easing: 'slide',
+    });
+});
+
+
+const dynamicText = document.querySelector("h1 span");
+const words = ["Global", "Masa Depan Indonesia"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+typeEffect();
+document.addEventListener("DOMContentLoaded", function() { 
+    const carousel = document.querySelector(".carousel"); 
+    const arrowBtns = document.querySelectorAll(".wrapper i"); 
+    const wrapper = document.querySelector(".wrapper"); 
+  
+    const firstCard = carousel.querySelector(".card"); 
+    const firstCardWidth = firstCard.offsetWidth; 
+  
+    let isDragging = false, 
+        startX, 
+        startScrollLeft, 
+        timeoutId; 
+  
+    const dragStart = (e) => {  
+        isDragging = true; 
+        carousel.classList.add("dragging"); 
+        startX = e.pageX; 
+        startScrollLeft = carousel.scrollLeft; 
+    }; 
+  
+    const dragging = (e) => { 
+        if (!isDragging) return; 
+        const newScrollLeft = startScrollLeft - (e.pageX - startX); 
+        if (newScrollLeft <= 0 || newScrollLeft >=  
+            carousel.scrollWidth - carousel.offsetWidth) { 
+            isDragging = false; 
+            return; 
+        } 
+        carousel.scrollLeft = newScrollLeft; 
+    }; 
+  
+    const dragStop = () => { 
+        isDragging = false;  
+        carousel.classList.remove("dragging"); 
+    }; 
+  
+    const autoPlay = () => { 
+        if (window.innerWidth < 800) return;  
+        const totalCardWidth = carousel.scrollWidth; n 
+        const maxScrollLeft = totalCardWidth - carousel.offsetWidth; 
+        if (carousel.scrollLeft >= maxScrollLeft) return; 
+          
+        timeoutId = setTimeout(() =>  
+            carousel.scrollLeft += firstCardWidth, 2500); 
+    }; 
+  
+    carousel.addEventListener("mousedown", dragStart); 
+    carousel.addEventListener("mousemove", dragging); 
+    document.addEventListener("mouseup", dragStop); 
+    wrapper.addEventListener("mouseenter", () =>  
+        clearTimeout(timeoutId)); 
+    wrapper.addEventListener("mouseleave", autoPlay); 
+  
+    // Add event listeners for the arrow buttons to  
+    // scroll the carousel left and right 
+    arrowBtns.forEach(btn => { 
+        btn.addEventListener("click", () => { 
+            carousel.scrollLeft += btn.id === "left" ?  
+                -firstCardWidth : firstCardWidth; 
+        }); 
+    }); 
+}); 
