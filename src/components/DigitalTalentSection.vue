30368061d01cafe39bc971ec50a965be0026af04
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-07-09 22:09:13 +0700

    feat(testimony): add testimoni section

---
+++ src/components/DigitalTalentSection.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col justify-center items-center py-8">
+    <h1 class="text-3xl font-semibold text-blue-950 text-center mb-4">Talenta Digital SMKDEV</h1>
+    <img
+      src="@/assets/Professional-5-Steps-SMKDEV-Build-Digital-Talent-2.png"
+      alt="SMK Dev digital Talent"
+      class="w-[800px] px-8"
+    />
+  </div>
+</template>
--- src/components/WhyChooseUsSection.vue
+++ src/components/WhyChooseUsSection.vue
@@ -4,8 +4,10 @@ import PencilIcon from './icons/PencilIcon.vue'
 import MentorIcon from './icons/MentorIcon.vue'
 </script>
 <template>
-  <div class="flex flex-col md:flex-row distance justify-center items-center gap-8 py-8">
-    <div class="flex flex-col w-1/2 gap-4">
+  <div
+    class="flex flex-col-reverse md:flex-row distance justify-center items-center gap-8 py-8 w-full"
+  >
+    <div class="flex flex-col w-full lg:w-1/2 gap-4">
       <div class="flex gap-6 items-center border rounded-xl shadow-lg p-4 px-6 h-32">
         <ReadBookIcon class="w-10 h-10 fill-blue-600" />
         <div>
@@ -32,11 +34,11 @@ import MentorIcon from './icons/MentorIcon.vue'
         </div>
       </div>
     </div>
-    <div class="w-1/2">
+    <div class="w-full lg:w-1/2">
       <h2 class="text-start text-3xl font-semibold text-blue-950">
-        MENGAPA HARUS MEMILIH BELAJAR DI <span class="text-blue-600">SMKDEV</span>
+        MENGAPA HARUS MEMILIH BELAJAR DI <span class="text-blue-600">SMKDEV?</span>
       </h2>
-      <p class="mt-4 text-gray-600">
+      <p class="mt-4 text-gray-600 px-8 text-lg">
         Pengalaman belajarmu jauh lebih menyenangkan dengan perpaduan belajar teori dan capstone
         project yang dapat menjadi portofolio pribadimu. Tidak lupa, Kamu juga dapat belajar
         langsung dari expert mentor dari dunia industri.
