c180c234220a2a51a28c92dfa7eeb829c05968e0
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-07-10 01:26:19 +0700

    feat: add new section, testimoni & mentor

--- smkdev-landing-page/src/components/Main.svelte
+++ smkdev-landing-page/src/components/Main.svelte
@@ -1,6 +1,8 @@
 <script>
     import Step from "./Step.svelte";
     import { onMount } from 'svelte';
+    import Testimoni from "./Testimoni.svelte";
+    import Mentor from './Mentor.svelte';
     // Import Swiper bundle with all modules installed
     import Swiper from 'swiper/bundle';
 
@@ -55,40 +57,172 @@
             icon: "fa-solid fa-person-chalkboard",
             description: "Dibimbing langsung oleh mentor expert dari industri digital"
         }
-    ]
+    ];
     let talents = [
         {
-            title: "Kurikulum",
-            icon: "fa-solid fa-globe fa-2x",
-            description: "Kurikulum berstandar industri global, senantiasa diperbaharui"
+            description: "Daftar ke program pelatihan SMKDEV"
         },
         {
-            title: "Metode",
-            icon: "fa-solid fa-pencil fa-2x",
-            description: "Metode belajar project-based learning, dapat menjadi portfolio siswa"
+            description: "Mengikuti semua program pelatihan sampai tuntas"
         },
         {
-            title: "Mentor",
-            icon: "fa-solid fa-person-chalkboard",
-            description: "Dibimbing langsung oleh mentor expert dari industri digital"
+            description: "Terdaftar pada Talent Pool SMKDEV"
+        },
+        {
+            description: "Mengikuti program magang/kerja di Partner Industri SMKDEV"
+        },
+        {
+            description: "Saya telah menjadi talenta digital handal"
         }
+    ];
+    let testimonies = [
+        {
+            name: "Guruh Maulana",
+            job: "STMIK IKMI Cirebon",
+            message: "Pada program SMKDEV Scholarship ini memberi saya kesempatan untuk berkenalan dan berinteraksi langsung dengan pengajar profesional di bidang data analyst. Pada program ini juga saya mendapatkan wawasan tentang berbagai peluang karier dalam analisis data dan teknologi informasi dan membantu saya merencanakan masa depan karir di data analyst.",
+        },
+        {
+            name: "Tiara Febrianie",
+            job: "SMKN 11 Malang",
+            message: "Scholarship SMKDEV itu kece banget, walaupun aku dapat Scholarship 100%, kualitas kelasnya gak sembarangan. Kelas meet yang diadain malem bikin aku gak bingung bagi waktu sama kegiatan lainnya. Terus, kak Samuel juga ramah banget. Gak kayak yang bikin aku jadi 'segan bertanya'. Materinya juga oke, bikin otakku berasa pening nyusun kode buat submission. Terima kasih, SMKDEV!",
+        },
+        {
+            name: "Rizal Maulana",
+            job: "SMK NEGERI 1 CIMAHI",
+            message: "SMKDEV Scholarship ini sangat menarik, karena memiliki penyampaian yang seru dan mudah dipahami. Selain itu juga di dalam bootcamp ini selalu ada di sela-sela kegiatan mengadakan mini games berhadiah",
+        },
+        {
+            name: "Rahmatulloh",
+            job: "Pengajar Pondok Al Fatih Depok",
+            message: "Kebetulan aku switch career dari guru ngaji, terus aku baca perkembangan sekarang tuh karir yang dibutuhkan di dunia IT itu gak jauh dari programming ataupun olah data. Karena kab data itu “gold”nya zaman now. Nah kebetulan SMKDEV ada Bootcamp dan webinar Data Analyst. Saya tertariknya sama materi Chrun yang ternyata bisa jadi dasar pengambilan keputusan dalam menentukan strategi untuk meningkatkan pangsa pasar atau meningkatkan loyalty customer kita.",
+        },
+        {
+            name: "Hasban Fardani",
+            job: "Siswa SMKN 11 Bandung",
+            message: "Seru! Bisa ketemu orang-orang hebat yang udah bikin banyak project keren. Bisa saling sharing pengalaman ngoding juga. Pokoknya seru deh!",
+        },
+        {
+            name: "Karel Trisnanto Utomo",
+            job: "Frontend Web Developer",
+            message: "Sejak bergabung dengan komunitas SMKDEV saya mendapat pengalaman yang banyak terlebih lagi melalui banyak event seperti community bonding talk yang membantu mengembangkan soft skill saya, dengan adanya coding challenge juga membuat saya bersemangat untuk meningkatkan skill pemrograman khususnya problem solving dengan lebih baik lagi, terimakasih SMKDEV dan tim",
+        },
+        {
+            name: "Asep Dwi Saputra",
+            job: "Teknik Informatika STMIK Widya Utama",
+            message: "Menjadi bagian keluarga SMKDev membantu mengembangkan keterampilan pemrograman saya (melalui event Coding Challenge misalnya). Selain itu, dukungan pemecahan masalah teknis, diskusi bersama, berbagi pengalaman (di Comunity Bounding misalnya) dan kesempatan berkolaborasi dalam sebuah proyek menjadi hal menarik yang tidak bisa saya lewatkan.",
+        },
+        {
+            name: "Muhammad Iqbal Pasha Al Farabi",
+            job: "Teknik Infomatika ITENAS",
+            message: "Merupakan pengalaman yang sangat menyenangkan bagi saya, mencoba hal baru dan memecahkan suatu masalah. Selain itu, saya juga mendapatkan kesempatan untuk menjalin banyak hubungan dan koneksi dengan orang-orang yang memiliki minat serupa. Dengan ini, saya dapat berbagi pengetahuan dan pengalaman saya dengan orang lain, serta menerima wawasan dan ilmu baru melalui berbagai perspektif yang berbeda."
+        },
+    ];
+    let mentors = [
+        {
+            name: "Ibnu Sina Wardy",
+            position: "CEO",
+            job: "GITS.ID",
+            img: "/images/ibnuS.png",
+            twitter: "#",
+            github: "#",
+            linkedin: "#",
+        },
+        {
+            name: "Helmi Adi Prasetyo",
+            position: "SMKDEV",
+            job: "Backend Developer",
+            img: "/images/helmi.png",
+            twitter: "#",
+            github: "#",
+            linkedin: "#",
+        },
+        {
+            name: "Samuel Pandohan T G",
+            position: "SMKDEV",
+            job: "Curriculum Developer",
+            img: "/images/samuel.png",
+            twitter: "#",
+            github: "#",
+            linkedin: "#",
+        },
+        {
+            name: "Rachmawati Ari T",
+            position: "SMKDEV",
+            job: "Curriculum Developer",
+            img: "/images/rachmawati.png",
+            twitter: "#",
+            github: "#",
+            linkedin: "#",
+        },
+        {
+            name: "Sudaryatno",
+            position: "CTO",
+            job: "GITS.ID",
+            img: "/images/sudaryatno.png",
+            twitter: "#",
+            github: "#",
+            linkedin: "#",
+        },
     ]
-
     onMount(() => {
-        const swiper = new Swiper(".centered-slide-carousel", {
+        const swiper = new Swiper(".mitra-carousel", {
         centeredSlides: true,
         paginationClickable: true,
         loop: true,
         spaceBetween: 30,
         slideToClickedSlide: true,
         pagination: {
-            el: ".centered-slide-carousel .swiper-pagination",
+            el: ".mitra-carousel .swiper-pagination",
             clickable: true,
         },
         breakpoints: {
             
             1028: {
             slidesPerView: 5,
+            spaceBetween: 10
+            },
+            0: {
+            slidesPerView: 3,
+            spaceBetween: 0
+            }
+        }
+      });
+        const swiperTesti = new Swiper(".testi-carousel", {
+        centeredSlides: true,
+        paginationClickable: true,
+        loop: true,
+        spaceBetween: 30,
+        slideToClickedSlide: true,
+        pagination: {
+            el: ".testi-carousel .swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            
+            1028: {
+            slidesPerView: 3,
+            spaceBetween: 10
+            },
+            0: {
+            slidesPerView: 1,
+            spaceBetween: 0
+            }
+        }
+      });
+        const swiperMentor = new Swiper(".mentor-carousel", {
+        centeredSlides: true,
+        paginationClickable: true,
+        loop: true,
+        spaceBetween: 30,
+        slideToClickedSlide: true,
+        pagination: {
+            el: ".mentor-carousel .swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            
+            1028: {
+            slidesPerView: 3,
             spaceBetween: 10
             },
             0: {
@@ -136,7 +270,7 @@
         </div>
         <div class="flex justify-center bg-violet-50 px-6 lg:px-[300px]">
             <div class="w-full relative">
-                <div class="swiper centered-slide-carousel swiper-container relative ">
+                <div class="swiper mitra-carousel swiper-container relative ">
                     <div class="swiper-wrapper h-max relative">
                         <div class="swiper-slide">
                             <div class="bg-violet-50 rounded-2xl h-36 flex justify-center items-center">
@@ -267,32 +401,90 @@
             </p>
         </div>
     </section>    
-    <!-- About -->
+    <!-- Talenta -->
     <section
     id="about"
-    class="py-8 lg:py-10 pt-7 lg:pt-9 flex flex-col gap-10 sm:gap-20 md:gap-24 relative lg:px-[300px]">
-    <div class="flex flex-col gap-2 text-center relative  before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-400 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-400 py-4">
+    class="py-6 lg:py-8 flex flex-col gap-2 sm:gap-10 md:gap-6 px-10 lg:px-[300px]">
+    <div class="flex flex-col gap-2 text-center relative  before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-100 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-100 py-4">
         <h3 class="font-semibold text-xl sm:text-2xl md:text-4xl">
             Talenta Digital <span class="text-violet-400">SMKDEV</span> 
         </h3>
     </div>
-    <div class="flex flex-col gap-20 w-full bg-indigo-100 grid align-items-center">
-        {#each talents as talent, index}
-            <div class="flex gap-2 sm:gap-5">
-                <p
-                    class="poppins text-xl sm:text-2xl md:text-3xl text-slate-500 font-semibold"
-                >
-                    {index + 1}
-                </p>
-                <div class="flex flex-col gap-2 sm:gap-5">
-                    <h3 class="text-2xl sm:text-3xl md:text-5xl">
-                        {talent.name}
-                    </h3>
-                    <p>{talent.description}</p>
+    <div class="lg:flex lg:items-center ">
+        <div class="hidden lg:flex lg:items-center lg:w-1/2 lg:justify-center">
+            <img class="w-[20rem] h-[20rem] object-cover xl:w-[28rem] xl:h-[28rem] rounded-xl" src="/images/ilustration1.png" alt="">
+        </div>
+        <div class="w-full lg:space-y-4 lg:w-1/2 grid lg:grid-cols-1 grid-cols-2 lg:gap-0 gap-4">
+            {#each talents as talent, index}
+            <div class="md:flex md:items-start md:-mx-4  ">
+                <div class="md:mx-4 shadow-md w-full p-3 rounded-xl">
+                    <h1 class="text-sm sm:text-md md:text-lg">{talent.description}</h1>
                 </div>
             </div>
-        {/each}
+            {/each}
+        </div>
+        
     </div>
-</section>
+    </section>
+    <!-- Testimoni -->
+    <section class="py-3 lg:py-5 flex flex-col gap-2 bg-violet-50" id="testimoni">
+    <div class="flex flex-col items-center gap-2 text-center px-6 mb-6">
+        <h3 class="font-semibold text-3xl sm:text-4xl mb-3">
+            What <span class="poppins text-violet-400">they</span> said <span class="text-violet-400" id="cursor">
+                ?
+            </span>
+        </h3>
+        <h2 class="text-sm sm:text-md md:text-lg">
+            Mereka telah merasakan serunya belajar skill digital di SMKDEV. Apakah kamu ingin seperti mereka?
+        </h2>
+    </div>
+    <div class="flex justify-center bg-violet-50 px-6 lg:px-[300px]">
+        <div class="w-full relative">
+            <div class="swiper testi-carousel swiper-container">
+                <div class="swiper-wrapper h-max">
+                    {#each testimonies as testi }
+                    <div class="swiper-slide">
+                    <Testimoni testimoni = {testi}>
+                    </Testimoni>
+                    </div>
+                    {/each}
+                </div>
+            </div>
+        </div>    
+    </div>
+    </section>
+    <!-- Mentor -->
+    <section class="py-3 lg:py-5 flex flex-col gap-2 " id="Mentor">
+    <div class="flex flex-col items-center gap-2 text-center px-6 mb-6">
+        <h3 class="font-semibold text-3xl sm:text-4xl mb-3">
+            Expert Mentor
+        </h3>
+        <h2 class="text-sm sm:text-md md:text-lg">
+            SMKDEV menghadirkan expert dari industri untuk mendampingi proses belajarmu.
+        </h2>
+    </div>
+    <div class="flex justify-center px-6 lg:px-[300px]">
+        <div class="w-full relative">
+            <div class="swiper mentor-carousel swiper-container">
+                <div class="swiper-wrapper h-max">
+                    {#each mentors as mentor }
+                    <div class="swiper-slide">
+                    <Mentor mentor = {mentor}>
+                    </Mentor>
+                    </div>
+                    {/each}
+                </div>
+            </div>
+        </div>    
+    </div>
+    </section>
+    <!-- Article -->
+    <section class="py-10 lg:py-28 flex flex-col gap-16 px-6 lg:px-[300px]" id="learning">
+        <div class="flex flex-col gap-2 text-center">
+            <h3 class="font-semibold text-3xl sm:text-4xl">
+                Artikel Pilihan <span class="poppins text-violet-400">SMKDEV</span> 
+            </h3>
+        </div>
+    </section>
 </main>
 
---
+++ smkdev-landing-page/src/components/Testimoni.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let testimoni;
+</script>
+
+<figure class="flex flex-col items-center justify-center p-8 mx-4 lg:mx-0 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-white">
+    <blockquote class="max-w-2xl mx-auto mb-4 text-dark  lg:mb-8 dark:text-drak">
+        <p class="my-4">"{testimoni.message}"</p>
+    </blockquote>
+    <figcaption class="flex items-center justify-center ">
+        <img class="rounded-full w-9 h-9" src="/images/profile.png" alt="{testimoni.name}">
+        <div class="space-y-0.5 font-medium text-left rtl:text-right ms-3">
+            <div class="font-semibold ">{testimoni.name}</div>
+            <div class="text-sm  ">{testimoni.job}</div>
+        </div>
+    </figcaption>    
+</figure>
