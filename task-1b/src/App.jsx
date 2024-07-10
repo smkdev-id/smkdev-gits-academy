ae63b454f1fb5223d6bea7254429276b26357298
rhyssh <reikiaziz30@gmail.com>
2024-07-10 21:28:44 +0700

    docs:create environtment

---
+++ task-1b/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    </>
+  )
+}
+
+export default App
