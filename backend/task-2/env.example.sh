d34082e0ddd515091ddc0e70ca2d558b5c076a1e
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-07-18 13:34:38 +0700

    feat: add project structure

---
+++ backend/task-2/env.example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Database Configuration
+export APP_DB_DRIVER=
+export APP_DB_HOST=
+export APP_DB_PORT=
+export APP_DB_USER=
+export APP_DB_PASSWORD=
+export APP_DB_NAME=
+export APP_SSL_MODE=
+export APP_TIME_ZONE=
+
+# Api Configuration
+export APP_API_HOST=
+export APP_API_PORT=
