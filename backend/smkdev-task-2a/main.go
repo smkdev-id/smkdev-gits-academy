ceed687b4b1753c97e8ea3bddd3a4ff50f29f199
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-07-20 18:33:33 +0700

    delete middleware logger and recovery

--- backend/smkdev-task-2a/main.go
+++ backend/smkdev-task-2a/main.go
@@ -5,29 +5,27 @@ import (
 	"golang-crud/handlers"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const (
-	SECRET_KEY string = "secret"
-	DB_NAME    string = "todo.db"
-)
+// database name
+const DB_NAME    string = "todo.db"
 
 
 func main() {
+	// echo initialization
 	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
 
+	// server connection to sqlite3 database
 	db, err := db.GetConnection(DB_NAME)
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
 	defer db.Close()
 
+	// route initialization
 	handlers.Route(e, db)
 
+	// Echo server on port 8080
 	e.Logger.Fatal(e.Start(":8080"))
 }
